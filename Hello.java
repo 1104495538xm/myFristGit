a93c69e6e75df4dac9149f537f0223fb67e3e17d
xumeng0 <55583753+xumeng0@users.noreply.github.com>
2019-09-22 17:04:30 +0800

    Update Hello.java
    delete massage

--- Hello.java
+++ Hello.java
@@ -5,7 +5,5 @@ public class Hello{
 	    system.out.println("my third..........");
 	    system.out.println("--------");
 	    system.out.println("33333333");
-            system.out.println("張攀是大傻子");
-		system.out.println("張攀是大傻子");
-		system.out.println("張攀是大傻子");
+            
 	}
