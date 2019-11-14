ea98b264d640ac6024149a1ba7780aac5a174145
xumeng0 <55583753+xumeng0@users.noreply.github.com>
2019-11-15 00:36:57 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,6 @@ public class Hello{
 	
 	    system.out.println("my frist git2---------------");
 	    system.out.println("my third..........");
-	    system.out.println("--------");
-	    system.out.println("33333333");
+	    
             
 	}
